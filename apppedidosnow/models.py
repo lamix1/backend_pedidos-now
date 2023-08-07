cf4128d9afc8741f04e9c57809fea1602b6c67fd
lamix1 <camilacristina328santoss@gmail.com>
2023-08-07 10:41:53 -0300

    Criando o model Produto

--- apppedidosnow/models.py
+++ apppedidosnow/models.py
@@ -5,10 +5,13 @@ class Categoria(models.Model):
     def __str__(self):
         return self.descricao
 
-class (models.Model):
-    nome = models.CharField(max_length=100)
-    site = models.URLField(null=True, blank=True)
-
+class Produto(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="produtos"
+    )
     def __str__(self):
-        return self.nome
+        return f"{self.titulo} ({self.quantidade})"
 
