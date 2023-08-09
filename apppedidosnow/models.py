8043847450125bc9df45397b92085623f67b0e15
lamix1 <camilacristina328santoss@gmail.com>
2023-08-09 11:46:38 -0300

    Corrigido a table Produto

--- apppedidosnow/models.py
+++ apppedidosnow/models.py
@@ -9,9 +9,10 @@ class Produto(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    gramas = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
     categoria = models.ForeignKey(
         Categoria, on_delete=models.PROTECT, related_name="produtos"
     )
     def __str__(self):
-        return f"{self.titulo} ({self.quantidade})"
+        return f"{self.titulo}"
 
