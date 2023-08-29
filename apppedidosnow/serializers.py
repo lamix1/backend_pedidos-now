7adfc4aaa890e17f0a54e6122cfa959f8d3c0013
luizatorrens <luiza.torrens.2006@gmail.com>
2023-08-29 16:31:33 -0300

    Mudança no banco

--- apppedidosnow/serializers.py
+++ apppedidosnow/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 
 from apppedidosnow.models import Categoria, Produto
 
@@ -8,6 +8,7 @@ class CategoriaSerializer(ModelSerializer):
         fields = "__all__"
 
 class ProdutoSerializer(ModelSerializer):
+    categoria = CharField(source="categoria.descricao", read_only=True)
     class Meta:
         model = Produto
         fields = "__all__"
