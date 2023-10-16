7865a6ad566a3631fd89a8404a5bd06585169370
lamix1 <camilacristina328santoss@gmail.com>
2023-10-16 10:26:38 -0300

    Incluindo o projeto no flow

--- pedidosnow/settings.py
+++ pedidosnow/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -172,3 +173,7 @@ SPECTACULAR_SETTINGS = {
 
 # AUTH_USER_MODEL = "usuario.Usuario"
 
+...
+if MODE == "PRODUCTION":
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
